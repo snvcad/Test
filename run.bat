71c37cbc492e6c916a96e123fda0543377549e54
snvcad <saji.varghese@marriott.com>
2019-12-13 11:26:58 -0500

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,5 @@
+set PATH=C:\windows\system32;C:\windows;C:\windows\System32\Wbem;C:\windows\System32\WindowsPowerShell\v1.0\;C:\Product\Git\cmd;C:\Users\svarg569\AppData\Local\GitHubDesktop\bin;C:\Product\Java\jdk-13.0.1\bin;C:\Product\Java\jdk-13.0.1\lib;.
+
+
+javac -classpath  ".;C:\Product\Java\jdk-13.0.1\plugins\commons-io-2.6.jar" -deprecation UpdateContent.java
+
